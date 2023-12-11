1f4670aa4ae9c8564fd909671066980238929e2e
Darth-Tenebros <yolisapingilili2@gmail.com>
2023-12-11 08:06:31 +0200

    docs

--- Controllers/TaskManagerController.cs
+++ Controllers/TaskManagerController.cs
@@ -1,4 +1,5 @@
 
+using System.Net.Mime;
 using Microsoft.AspNetCore.Mvc;
 using TaskManager.DatabaseComms;
 using TaskManager.Models;
@@ -8,6 +9,8 @@ namespace TaskManager.Controllers
 {
     [ApiController]
     [Route("api/")]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Consumes(MediaTypeNames.Application.Json)]
     public class TaskManagerController : Controller
     {
         private readonly TaskManagerDbContext _taskManagerDbContext;
@@ -17,15 +20,28 @@ namespace TaskManager.Controllers
         }
 
         #region UserControls
+        
+        /// <summary>
+        /// Retrieve all users in the database
+        /// </summary>
+        /// <returns>A list with all users.</returns>
         [HttpGet]
         [Route("user/GetUsers")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult GetUsers()
         {
             return Ok(_taskManagerDbContext.Users.ToList());
         }
         
+        /// <summary>
+        /// Retrieve a user by the Id
+        /// </summary>
+        /// <param name="id">The ID of the user.</param>
+        /// <returns>A user object.</returns>
         [HttpGet]
         [Route("user/GetUser/{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult GetUser([FromRoute] Guid id)
         {
             var user = _taskManagerDbContext.Users.Find(id);
@@ -37,8 +53,14 @@ namespace TaskManager.Controllers
             return NotFound($"user with id {id} not found");
         }
 
+        /// <summary>
+        /// Add a new user to the database
+        /// </summary>
+        /// <param name="user">Object with the supplied user data</param>
+        /// <returns>The new user.</returns>
         [HttpPost]
         [Route("user/AddUser")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult AddUser(UserRequest user)
         {
             var newUser = new User()
@@ -54,8 +76,16 @@ namespace TaskManager.Controllers
             return Ok(newUser);
         }
 
+        /// <summary>
+        /// Update the user with the given guid
+        /// </summary>
+        /// <param name="id">The ID of the user.</param>
+        /// <param name="userRequest">Object with the supplied user data</param>
+        /// <returns>The updated user.</returns>
         [HttpPut]
         [Route("user/UpdateUser/{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult UpdateUser([FromRoute] Guid id, UserRequest userRequest)
         {
             var user = _taskManagerDbContext.Users.Find(id);
@@ -72,8 +102,15 @@ namespace TaskManager.Controllers
             return NotFound($"No user with id {id}");
         }
 
+        /// <summary>
+        /// Delete the user with the given id.
+        /// </summary>
+        /// <param name="id">The ID of the user.</param>
+        /// <returns>The deleted user.</returns>
         [HttpDelete]
         [Route("user/delete{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult DeleteUser([FromRoute] Guid id)
         {
             var user = _taskManagerDbContext.Users.Find(id);
@@ -89,15 +126,28 @@ namespace TaskManager.Controllers
         #endregion
 
         #region TaskControls
+        
+        /// <summary>
+        /// Retrieve all tasks in the database
+        /// </summary>
+        /// <returns>A list of tasks.</returns>
         [HttpGet]
         [Route("task/GetTasks")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult GetTasks()
         {
             return Ok(_taskManagerDbContext.Tasks.ToList());
         }
 
+        /// <summary>
+        /// Retrieve the task with the given id
+        /// </summary>
+        /// <param name="id">The ID of the task.</param>
+        /// <returns>A task object.</returns>
         [HttpGet]
         [Route("task/GetTask/{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult GetTask([FromRoute] Guid id)
         {
             var task = _taskManagerDbContext.Tasks.Find(id);
@@ -109,8 +159,15 @@ namespace TaskManager.Controllers
             return NotFound($"task with id {id} not found");
         }
 
+        /// <summary>
+        /// Delete the task that is associated with the given user id
+        /// </summary>
+        /// <param name="userId">The id of the user.</param>
+        /// <returns>The deleted task.</returns>
         [HttpGet]
         [Route("task/GetTaskByUserId/{userId:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult GetTaskByUserId([FromRoute] Guid userId)
         {
             var userTasks = _taskManagerDbContext.Tasks.Where(task => task.Assignee == userId).ToList();
@@ -122,8 +179,14 @@ namespace TaskManager.Controllers
             return NotFound($"no tasks by user with id {userId}");
         }
 
+        /// <summary>
+        /// Add a new task to the database.
+        /// </summary>
+        /// <param name="task">Object with the supplied data.</param>
+        /// <returns>The new task.</returns>
         [HttpPost]
         [Route("task/AddTask")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult AddTask(TaskRequest task)
         {
             var newTask = new Task()
@@ -141,8 +204,16 @@ namespace TaskManager.Controllers
             return Ok(newTask);
         }
 
+        /// <summary>
+        /// Update the task with the given id.
+        /// </summary>
+        /// <param name="id">Object with the supplied data.</param>
+        /// <param name="taskRequest">Object with the supplied task data.</param>
+        /// <returns>The updated task.</returns>
         [HttpPut]
         [Route("task/UpdateTask/{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult UpdateTask([FromRoute] Guid id, TaskRequest taskRequest)
         {
             var task = _taskManagerDbContext.Tasks.Find(id);
@@ -160,8 +231,15 @@ namespace TaskManager.Controllers
             return NotFound($"No task with id {id}");
         }
 
+        /// <summary>
+        /// Delete the task with the given id.
+        /// </summary>
+        /// <param name="id">The id of the task.</param>
+        /// <returns>The deleted task.</returns>
         [HttpDelete]
         [Route("task/DeleteTask/{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult DeleteTask([FromRoute] Guid id)
         {
             var task = _taskManagerDbContext.Tasks.Find(id);
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using TaskManager.DatabaseComms;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +10,31 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo()
+        {
+            Title = "TaskManager",
+            Description = "Task Api",
+            Contact = new OpenApiContact
+            {
+                Name = "Yolisa",
+                Email = "yolisapingilili2@gmail.com",
+                Url = new Uri("https://github.com/Darth-Tenebros")
+            },
+            License = new OpenApiLicense
+            {
+                Name = "no",
+                Url = new Uri("https://github.com/Darth-Tenebros")
+            },
+            Version = "v1"
+        });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+    
 
 // builder.Services.AddDbContext<TaskManagerDbContext>(options => options.UseInMemoryDatabase("TaskManagerStore"));
 builder.Services.AddDbContext<TaskManagerDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("TaskDBConn")));
